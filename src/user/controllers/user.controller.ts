b4ac09933070c581f283631fd8c7195ad22efee0
Johaire Salih <salihjohaire@gmail.com>
2025-11-07 19:35:28 +0800

    created patch function for service and controller

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { GetUsersQueryDto } from '../dto/user-query.dto';
 import { Roles } from 'src/auth/decorators/role.decorator';
 import { UserRole } from '../interfaces/user-role.enum';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { UserPatchDto } from '../dto/patch-user.dto';
 
 @Controller('user')
 @ApiBearerAuth()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // Get all users - ADMIN only
   @Roles(UserRole.ADMIN)
   @Get()
   getUsers(@Query() query: GetUsersQueryDto) {
     return this.userService.getUsers(query);
+  }
+
+  // Update user - ADMIN only
+  @Roles(UserRole.ADMIN)
+  @Patch(':id')
+  updateUserStatus(
+    @Param('id') id: string,
+    @Body() userPatchDto: UserPatchDto,
+  ) {
+    return this.userService.patch(id, userPatchDto);
   }
 }
