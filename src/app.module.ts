5f0c80e5ae1c02824e4b467f4cdd58c8db2a0e61
fencim <fencim@gmail.com>
2025-11-08 11:06:28 +0800

    feat: add environment configuration for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { AuthGuard } from './auth/guards/auth.guard';
     }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
+        const isLocal = configService.get<string>('environment') === 'development';
         return {
           type: 'postgres',
           host: configService.get('database.host') || 'localhost',
@@ -31,7 +32,7 @@ import { AuthGuard } from './auth/guards/auth.guard';
           database: configService.get('database.dbName') || 'bansay_db',
           entities: [Student, User, Officer],
           synchronize: true,
-          extra: {
+          extra: isLocal ? undefined: {
             ssl: {
               rejectUnauthorized: false, 
             },
