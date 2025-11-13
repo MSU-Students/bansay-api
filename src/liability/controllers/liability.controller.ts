2e62268998eb1218967a5541fc0fcd5d38f37ff8
Johaire Salih <salihjohaire@gmail.com>
2025-11-13 10:04:24 +0800

    implemented controller for liability module

---
+++ src/liability/controllers/liability.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Param,
+  Get,
+} from '@nestjs/common';
+import { CreateLiabilityDto } from '../dto/create-liability.dto';
+import { LiabilityService } from '../services/liability.service';
+import { UserRole } from 'src/user/interfaces/user-role.enum';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import type { RequestWithUser } from 'src/auth/types/request-with-user.interface';
+import { Liability } from '../entities/liability.entity';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+@Controller('liability')
+@ApiBearerAuth()
+export class LiabilityController {
+  constructor(private readonly liabilityService: LiabilityService) {}
+
+  @Post()
+  @Roles(UserRole.OFFICER)
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body() createLiabilityDto: CreateLiabilityDto,
+    @Req() req: RequestWithUser,
+  ): Promise<any> {
+    const issuerId = req.user.userId;
+
+    const liability = (await this.liabilityService.createLiability(
+      createLiabilityDto,
+      Number(issuerId),
+    )) as Liability;
+
+    return {
+      message: 'Liability created successfully',
+      liability,
+    };
+  }
+
+  @Get()
+  @Roles()
+  async findOne(@Param('id') id: number): Promise<{
+    message: string;
+    liability: Liability;
+  }> {
+    const liability = await this.liabilityService.findLiabilityById(id);
+
+    return {
+      message: 'Liability retrieved successfully',
+      liability,
+    };
+  }
+}
