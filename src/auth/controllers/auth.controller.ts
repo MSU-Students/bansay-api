4a62e981866eff14617862e77d5b9312404f6564
AH Mangorangca <lavahotsnacks@gmail.com>
2025-11-14 16:29:27 +0800

    feat(auth): add protected /auth/me endpoint with GetUser decorator

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, Get } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { UserRegisterDto } from '../dto/user-register.dto';
 import { Public } from '../decorators/is-public.decorator';
 import { UserLoginDto } from '../dto/user-login.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetUser } from '../decorators/get-user.decorator';
+import type { JwtPayload } from '../types/jwt-payload.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +24,11 @@ export class AuthController {
   @Post('login')
   async login(@Body() userLoginDto: UserLoginDto) {
     return this.authService.login(userLoginDto);
+  }
+
+  @ApiBearerAuth()
+  @Get('me')
+  getMe(@GetUser() user: JwtPayload) {
+    return user;
   }
 }
