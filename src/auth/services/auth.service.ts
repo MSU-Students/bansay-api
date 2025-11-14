21380804c2880f414e96d3383c778de74d80df75
AH Mangorangca <lavahotsnacks@gmail.com>
2025-11-14 16:09:48 +0800

    fix(auth): load hidden password field during login validation

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -89,7 +89,20 @@ export class AuthService {
   private async validateUser(userLoginDto: UserLoginDto): Promise<User | null> {
     const { username, password } = userLoginDto;
 
-    const user = await this.userRepository.findOneBy({ username });
+    // const user = await this.userRepository.findOneBy({ username });
+    // from user.entity.ts, the code:
+    // @Column({ type: 'varchar', length: 128, select: false })
+    // "select: false" is a security feature, so using a normal database query like this one "findONeBy)", 
+    // TypeORM automatically hides the password so it's never accidentally sent to the frontend.
+    // because of that, the `user` object this query returns has user.password set to undefined.
+    // documenting this because it took a long time for me to debug this 
+    // as I encoutnered this 500 Internal Server Error on my Swagger when I was testing the login endpoint.
+    
+    const user = await this.userRepository
+    .createQueryBuilder('user')
+    .where('user.username = :username', { username })
+    .addSelect('user.password')
+    .getOne();
 
     // check 1: user exists
     // check 2: user status is active (not pending or disabled)
