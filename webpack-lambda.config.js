d0359eb3c2fcfe3b3d6c08139be7d7c7d89e11fe
fencim <fencim@gmail.com>
2025-11-19 19:28:26 +0800

    trigger build

---
+++ webpack-lambda.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+// List of modules that are not essential for Lambda and can be ignored
+// or assumed to be available in the Node.js runtime
+const lazyImports = [
+  '@nestjs/microservices/microservices-module',
+  '@nestjs/websockets/socket-module',
+  'cache-manager',
+  'class-validator',
+  'class-transformer',
+  // Add other non-essential lazy imports here as needed
+];
+
+module.exports = function (options, webpack) {
+  return {
+    // We target 'node' for a backend application
+    target: 'node',
+    // Set mode to 'production' for optimal bundling
+    mode: 'production',
+    
+    // The entry point should be your lambda handler file
+    entry: ['./src/lambda.ts'], //
+
+    // Output configuration
+    output: {
+      // The path to the output directory
+      path: path.join(__dirname, 'dist'),
+      // The name of the bundled file
+      filename: 'lambda.bundle.js',
+      // Specifies the module format for CommonJS (necessary for AWS Lambda)
+      libraryTarget: 'commonjs2',
+    },
+
+    // Enable source maps for easier debugging
+    devtool: 'source-map',
+
+    // Configure how modules are resolved (e.g., handling TypeScript extensions)
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+
+    // Module rules (loaders)
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+
+    // Plugins configuration
+    plugins: [
+      ...options.plugins,
+      // Ignore specific lazy-loaded modules to reduce bundle size
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+
+    // Externals: you generally want to bundle all dependencies for Lambda, 
+    // unless you use Lambda Layers. Setting to empty array ensures all node_modules are bundled.
+    externals: [],
+  };
+};
