099550f06a277a4e3c0f5dacfd5512fb9d145756
silc-o <bhussienaldondie@gmail.com>
2025-11-24 23:16:09 +0800

    added role to userlogindto

--- src/auth/dto/user-login.dto.ts
+++ src/auth/dto/user-login.dto.ts
@@ -14,4 +14,9 @@ export class UserLoginDto {
   @IsNotEmpty()
   @IsString()
   password: string;
+
+  @ApiProperty({ example: 'Student', description: 'Role of the user' })
+  @IsNotEmpty()
+  @IsString()
+  role: string;
 }
