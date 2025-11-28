6125749c4d1c8ab249515c47cb570cf507c71839
silc-o <bhussienaldondie@gmail.com>
2025-11-28 12:03:47 +0800

    removed duplicate logic on officer.service and student.service

--- src/user/services/officer.service.ts
+++ src/user/services/officer.service.ts
@@ -8,15 +8,6 @@ import { OfficerDto } from '../dto/officer.dto';
 export class OfficerService {
   constructor(@InjectRepository(Officer) private repo: Repository<Officer>) {}
   async create(officer: OfficerDto) {
-    // Check if officer already exists
-    const existing = await this.repo.findOne({
-      where: { idNumber: officer.idNumber },
-    });
-
-    if (existing) {
-      return existing;
-    }
-
     const record = this.repo.create({
       email: officer.email,
       idNumber: officer.idNumber,
@@ -27,9 +18,11 @@ export class OfficerService {
   }
 
   async findAll(filter?: Partial<OfficerDto>): Promise<OfficerDto[]> {
-    const result = filter ?   (await this.repo.find({
-      where: filter
-    })):  (await this.repo.find());
+    const result = filter
+      ? await this.repo.find({
+          where: filter,
+        })
+      : await this.repo.find();
     return result.map((record) => {
       return {
         id: record.id,
