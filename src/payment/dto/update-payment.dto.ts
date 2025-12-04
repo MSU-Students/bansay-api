7be5218c5701097022be420c53dcd0be10c9747d
Ahmad Badron <bdrnahmdg@gmail.com>
2025-12-04 22:41:47 +0800

    updated update payment dto to include status

--- src/payment/dto/update-payment.dto.ts
+++ src/payment/dto/update-payment.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePaymentDto } from './create-payment.dto';
+import { PaymentStatus } from '../types/payment-status.type';
+import { IsEnum, IsOptional } from 'class-validator';
 
-export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {}
+export class UpdatePaymentDto extends PartialType(CreatePaymentDto) {
+  @ApiProperty({
+    required: false,
+    enum: PaymentStatus,
+    example: PaymentStatus.PENDING_VERIFICATION,
+    description: 'Payment status',
+  })
+  @IsEnum(PaymentStatus)
+  @IsOptional()
+  status?: PaymentStatus;
+}
