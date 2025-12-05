3b9ca501ca2e5e72e858ef5317cf31921e6335b1
Ahmad Badron <ackx29sodo@gmail.com>
2025-12-05 14:39:57 +0800

    added User repository injection

--- src/appeal/appeal.module.ts
+++ src/appeal/appeal.module.ts
@@ -4,9 +4,10 @@ import { Appeal } from './entities/appeal.entity';
 import { Liability } from '@bansay/liability/entities/liability.entity';
 import { AppealService } from './services/appeal.service';
 import { AppealController } from './controllers/appeal.controller';
+import { User } from '@bansay/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Appeal, Liability])],
+  imports: [TypeOrmModule.forFeature([Appeal, Liability, User])],
   providers: [AppealService],
   controllers: [AppealController],
 })
